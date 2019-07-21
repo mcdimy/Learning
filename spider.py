8d6950b674c53ebe5b9ede3488d6af69d3518ea6
mcdimy <239090628@qq.com>
2019-07-21 14:25:58 +0800

    Add python script is folder

---
+++ spider.py
@@ -0,0 +1,20 @@
+
+from urllib import request
+
+class Spider():
+    url = 'http://www.le.com/ptv/vplay/27444833.html' 
+
+    def __fetch_content(self):
+        r  = request.urlopen(Spider.url)
+        htmls = r.read()
+        htmls = str(htmls,encoding='utf-8')
+        a = 1
+    def __analysis(self,htmls):
+        pass
+
+    def go(self):
+        self.__fetch_content()
+        self.__analysis(htmls)
+
+spider = Spider()
+spider.go()
